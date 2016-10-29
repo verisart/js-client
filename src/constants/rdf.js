9ed4d6498991b799287a2023c1df612262501bf0
scisci <dpriley@ucla.edu>
2016-10-28 21:03:21 -0400

    added rdf constants 0.0.8

---
+++ src/constants/rdf.js
@@ -0,0 +1,64 @@
+
+
+var RDF_SYNTAX_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+var RDF_SCHEMA_NS = "http://www.w3.org/2000/01/rdf-schema#";
+var XSD_NS = "http://www.w3.org/2001/XMLSchema#";
+var WGS_NS = "http://www.w3.org/2003/01/geo/wgs84_pos#";
+var OWL_NS = "http://www.w3.org/2002/07/owl#";
+var XSD_ANY_TYPE = XSD_NS + "anyType";
+var XSD_BOOLEAN = XSD_NS + "boolean";
+var XSD_DOUBLE = XSD_NS + "double";
+var XSD_INTEGER = XSD_NS + "integer";
+var XSD_FLOAT = XSD_NS + "float";
+var XSD_DECIMAL = XSD_NS + "decimal";
+var XSD_ANY_URI = XSD_NS + "anyURI";
+var XSD_STRING = XSD_NS + "string";
+var XSD_DATE = XSD_NS + "date";
+var XSD_GYEAR = XSD_NS + "gYear";
+var RDF_TYPE = RDF_SYNTAX_NS + "type";
+var RDF_FIRST = RDF_SYNTAX_NS + "first";
+var RDF_REST = RDF_SYNTAX_NS + "rest";
+var RDF_NIL = RDF_SYNTAX_NS + "nil";
+var RDF_PLAIN_LITERAL = RDF_SYNTAX_NS + "PlainLiteral";
+var RDF_XML_LITERAL = RDF_SYNTAX_NS + "XMLLiteral";
+var RDF_OBJECT = RDF_SYNTAX_NS + "object";
+var RDF_LANG_STRING = RDF_SYNTAX_NS + "langString";
+var RDF_LIST = RDF_SYNTAX_NS + "List";
+var WGS_LAT = WGS_NS + "lat";
+var WGS_LNG = WGS_NS + "long";
+var WGS_ALT = WGS_NS + "alt";
+var OWL_SAME_AS = OWL_NS + "sameAs";
+var RDFS_LABEL = RDF_SCHEMA_NS + "label";
+
+
+module.exports = {
+	RDF_SYNTAX_NS: RDF_SYNTAX_NS,
+	RDF_SCHEMA_NS: RDF_SCHEMA_NS,
+	XSD_NS: XSD_NS,
+	WGS_NS: WGS_NS,
+	OWL_NS: OWL_NS,
+	XSD_ANY_TYPE: XSD_ANY_TYPE,
+	XSD_BOOLEAN: XSD_BOOLEAN,
+	XSD_DOUBLE: XSD_DOUBLE,
+	XSD_INTEGER: XSD_INTEGER,
+	XSD_FLOAT: XSD_FLOAT,
+	XSD_DECIMAL: XSD_DECIMAL,
+	XSD_ANY_URI: XSD_ANY_URI,
+	XSD_STRING: XSD_STRING,
+	XSD_DATE: XSD_DATE,
+	XSD_GYEAR: XSD_GYEAR,
+	RDF_TYPE: RDF_TYPE,
+	RDF_FIRST: RDF_FIRST,
+	RDF_REST: RDF_REST,
+	RDF_NIL: RDF_NIL,
+	RDF_PLAIN_LITERAL: RDF_PLAIN_LITERAL,
+	RDF_XML_LITERAL: RDF_XML_LITERAL,
+	RDF_OBJECT: RDF_OBJECT,
+	RDF_LANG_STRING: RDF_LANG_STRING,
+	RDF_LIST: RDF_LIST,
+	WGS_LAT: WGS_LAT,
+	WGS_LNG: WGS_LNG,
+	WGS_ALT: WGS_ALT,
+	OWL_SAME_AS: OWL_SAME_AS,
+	RDFS_LABEL: RDFS_LABEL
+}
