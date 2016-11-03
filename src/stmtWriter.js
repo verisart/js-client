8a9159c3abc0e4337f7a9acb30aacaad7edbaa57
scisci <dpriley@ucla.edu>
2016-11-03 13:17:14 -0400

    adding more constants, updating statement writer api

--- src/stmtWriter.js
+++ src/stmtWriter.js
@@ -3,17 +3,41 @@ var rdf = require('./constants/rdf');
 var crm = require('./constants/crm');
 
 
-function StmtWriter(subjectTypeIRI) {
+function StmtWriter(subjectIRI, subjectTypeIRI) {
   this.context = rdf.contexts[rdf.defaultContext];
   this.stmt = {};
   this.completed = false;
-
-  this.stmt['@type'] = subjectTypeIRI;
 }
 
-function newActorStmtWriter() {
-  return new StmtWriter(crm.E39_ACTOR_IRI);
+StmtWriter.prototype.setSubjectIRI = function(iri) {
+  if (!iri || typeof iri !== 'string' || iri.length === 0) {
+    return;
+  }
+  
+  this.stmt['@id'] = iri;
 }
+
+StmtWriter.prototype.setSubjectTypeIRI = function(iri) {
+  if (!iri || typeof iri !== 'string' || iri.length === 0) {
+    return;
+  }
+
+  this.stmt['@type'] = iri;
+}
+
+function newCreateActorStmtWriter() {
+  var writer = new StmtWriter();
+  writer.setSubjectTypeIRI(crm.E39_ACTOR_IRI);
+  return writer;
+}
+
+function newUpdateActorStmtWriter(iri) {
+  var writer = new StmtWriter();
+  writer.setSubject(iri);
+  return writer;
+}
+
+
 
 function writeActorStmt(payload, cb) {
   var writer = newActorStmtWriter();
@@ -44,10 +68,20 @@ StmtWriter.prototype.addActorName = function(actorName) {
 
 
 StmtWriter.prototype.complete = function() {
+  if (!this.stmt['@id'] && ! this.stmt['@type']) {
+    // Can't complete not enough info
+    return;
+  }
+
   this.completed = true;
 };
 
 StmtWriter.prototype.frame = function(cb) {
+  if (!this.completed) {
+    cb('The writer is not complete');
+    return;
+  }
+
   jsonLD.compact(this.stmt, this.context, function(err, compacted) {
     cb(err, compacted);
   });
